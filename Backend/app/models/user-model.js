18edef8a7a08c7d4fcbb17e18ea8fc92ed54afe0
snehayendurineu <yenduri.s@northeastern.edu>
2024-04-19 04:31:44 -0400

    added type

--- Backend/app/models/user-model.js
+++ Backend/app/models/user-model.js
@@ -30,6 +30,12 @@ const UserSchema = new Schema(
       default:
         "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=",
     },
+    role: {
+      type: String,
+      required: [true, "User role is required"],
+      enum: ["Manager", "Developer", "QA"],
+      default: "Developer" // Optional: set a default role if desired
+    },
   },
   {
     versionKey: false,
